d0edf42d030ab9e08838dd4bfc54fb1dcfe7a525
Qxy <3022208737@qq.com>
2024-12-11 22:02:30 +0800

    第二次提交

---
+++ gmall2024-realtime2/realtime-dim/src/main/java/com/bw/gmall/realtime/dim/DimApp.java
@@ -0,0 +1,14 @@
+package com.bw.gmall.realtime.dim;
+
+/**
+ * @Author xqy
+ * @CreateTime 2024-12-11
+ */
+
+public class DimApp {
+    public static void main(String[] args) {
+        //1.基本环境准备
+        //2.设置并行度
+        //3.
+    }
+}
